c9ecc130aeccc36c8f7b97adfb8139abed7a536a
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2022-12-29 14:08:23 +0500

    video height

--- components/slider.js
+++ components/slider.js
@@ -136,7 +136,7 @@ function Slide({ video_url, title, sub_title, title_link }) {
         preload="auto"
         loop
         onLoadedMetadata={handleLoadedMetadata}
-        className="absolute z-10 object-cover w-full h-full"
+        className="z-10 object-cover w-full h-screen"
       >
         <source src={video_url} />
       </video>
