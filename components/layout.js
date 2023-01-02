951db9b2d594d75a2a8004292c754158039d7e8c
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-01-02 11:26:50 +0500

    Email

--- components/layout.js
+++ components/layout.js
@@ -11,16 +11,14 @@ export default function Layout({ children }) {
                     </svg>
                 </div>
             </div>
-
-            <Link href="mailto:info@abdullahawad.com" target="_blank" rel="nofollow" 
-                className="absolute 2xl:top-[11rem]  md:top-[10.2rem] top-[6rem] md:-right-10 -right-8 left-auto z-20 text-[#F0E6CC] FormulaCondensed md:text-[1.8rem] text-[1rem] transform rotate-90 flex "
+            <Link href="mailto:info@abdullahawad.com" target="_blank" rel="nofollow"
+                className="absolute 2xl:top-[11rem] tracking-[1.5px] md:top-[10.2rem] top-[6rem] md:-right-10 -right-8 left-auto z-20 text-[#F0E6CC] FormulaCondensed md:text-[1.8rem] text-[1rem] transform rotate-90 flex "
             >
-               info@abdullahawad.com
+                info@abdullahawad.com
             </Link>
             <div className="w-screen h-screen overflow-hidden h-screen-ios">
                 {children}
             </div>
-
         </main>
     );
 }
