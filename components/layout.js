3b802a80749b661d19e91c5eef178f98d381c2cf
Mufaqar Islam <mufaqar@gmail.com>
2024-11-03 14:26:36 +0500

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -21,7 +21,7 @@ export default function Layout({ children }) {
       </div>
       <Link
         href="mailto:info@abdullahawad.com"
-        target="_blank"
+        target=""
         rel="nofollow"
         className="absolute 2xl:top-[11rem] tracking-[1.5px] md:top-[10.2rem] top-[6rem] md:-right-10 -right-8 left-auto z-20 text-[#F0E6CC] FormulaCondensed md:text-[1.8rem] text-[1rem] transform rotate-90 flex "
       >
